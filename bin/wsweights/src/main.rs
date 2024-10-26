86435f934b67d6f2bf41e4aef3ba2ed447b55fd3
Erin Grasmick <erin@parity.io>
2024-10-26 15:37:55 +0100

    add block times

--- bin/wsweights/src/main.rs
+++ bin/wsweights/src/main.rs
@@ -1,9 +1,8 @@
-use serde::Serialize;
-use shared::registry::registered_paras;
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::env;
 use std::sync::Arc;
+use serde::Serialize;
 use subxt::{blocks::Block, OnlineClient, PolkadotConfig, utils::H256};
 use tokio::sync::{mpsc, RwLock};
 use tokio::task::JoinSet;
@@ -11,10 +10,10 @@ use tokio::time::{timeout, Duration};
 use warp::sse::Event;
 use warp::{http::Method, Filter};
 use types::{Parachain, RelayChain, WeightConsumption, Timestamp};
+use shared::registry::registered_paras;
 
 const LOG_TARGET: &str = "tracker";
 
-// Data structures for consumption updates sent over SSE
 #[derive(Serialize, Clone)]
 struct ConsumptionUpdate {
     para_id: u32,
@@ -23,6 +22,7 @@ struct ConsumptionUpdate {
     extrinsics_num: usize,
     authorities_num: usize,
     timestamp: Timestamp,
+    block_time_seconds: Option<f64>,
     ref_time: RefTime,
     proof_size: ProofSize,
     total_proof_size: f32,
@@ -42,59 +42,49 @@ struct ProofSize {
     mandatory: f32,
 }
 
-
 #[subxt::subxt(runtime_metadata_path = "../../artifacts/metadata.scale")]
 mod polkadot {}
 
-// Type alias for tracking connected SSE clients
 type Clients = Arc<RwLock<Vec<mpsc::UnboundedSender<Result<Event, Infallible>>>>>;
-
-// Type alias for the cache of the last messages
 type Cache = Arc<RwLock<HashMap<u32, ConsumptionUpdate>>>;
+type TimestampCache = Arc<RwLock<HashMap<u32, Timestamp>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::builder().filter_level(log::LevelFilter::Debug).init();
 
     let clients: Clients = Arc::new(RwLock::new(Vec::new()));
-    let cache: Cache = Arc::new(RwLock::new(HashMap::new())); // Initialize the cache
+    let cache: Cache = Arc::new(RwLock::new(HashMap::new()));
+    let timestamp_cache: TimestampCache = Arc::new(RwLock::new(HashMap::new()));
 
-    // Start SSE server for client connections
     let clients_clone = Arc::clone(&clients);
-    let cache_clone = Arc::clone(&cache); // Pass the cache to the SSE server
+    let cache_clone = Arc::clone(&cache);
     tokio::spawn(async move {
         if let Err(e) = start_sse_server(clients_clone, cache_clone).await {
             log::error!("SSE server encountered an error: {:?}", e);
         }
     });
 
-    // Start tracking parachain data and send updates to SSE clients
-    if let Err(e) = start_tracking(0, clients.clone(), cache.clone()).await {
+    if let Err(e) = start_tracking(0, clients.clone(), cache.clone(), timestamp_cache.clone()).await {
         log::error!("Tracking system encountered an error: {:?}", e);
     }
 
     Ok(())
 }
 
-// Start SSE server to accept client connections
 async fn start_sse_server(
     clients: Clients,
     cache: Cache,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    dotenv::dotenv().ok(); // Load environment variables from `.env` file (optional)
+    dotenv::dotenv().ok();
 
-    // Read the IP address and port from environment variables, or default to "127.0.0.1:9001"
     let ip = env::var("SSE_IP").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("SSE_PORT").unwrap_or_else(|_| "9001".to_string());
-
-    // Combine IP and port into the full address
-    let addr_str = format!("{}:{}", ip, port);
-    let addr: std::net::SocketAddr = addr_str.parse()?;
+    let addr: std::net::SocketAddr = format!("{}:{}", ip, port).parse()?;
 
     let clients_filter = warp::any().map(move || clients.clone());
     let cache_filter = warp::any().map(move || cache.clone());
 
-    // Configure CORS
     let cors = warp::cors()
         .allow_any_origin()
         .allow_methods(vec![Method::GET])
@@ -111,17 +101,13 @@ async fn start_sse_server(
         .and(cache_filter)
         .and_then(
             |clients: Clients, cache: Cache| async move {
-                // Create an unbounded channel to send SSE events
                 let (tx, rx) = mpsc::unbounded_channel();
 
-                // Send cached messages to the new client
                 {
                     let cache = cache.read().await;
                     for (_, message) in cache.iter() {
                         let data = serde_json::to_string(message).unwrap();
-                        // Generate an event ID based on para_id and block_number
-                        let event_id = format!("{}-{}", message.para_id, message.block_number);
-                        // Create the SSE event with the same format as regular messages
+                        let event_id = format!("{}-{}-{}", message.relay, message.para_id, message.block_number);
                         let sse_event = Event::default()
                             .id(event_id)
                             .event("consumptionUpdate")
@@ -129,13 +115,8 @@ async fn start_sse_server(
                         let _ = tx.send(Ok(sse_event));
                     }
                 }
-                // Add the sender to the list of clients
                 clients.write().await.push(tx);
-
-                // Create a stream from the receiver
                 let stream = tokio_stream::wrappers::UnboundedReceiverStream::new(rx);
-
-                // Reply using server-sent events
                 Ok::<_, Infallible>(warp::sse::reply(warp::sse::keep_alive().stream(stream)))
             },
         )
@@ -146,23 +127,23 @@ async fn start_sse_server(
     Ok(())
 }
 
-// Start tracking parachain consumption and send updates to SSE clients
 async fn start_tracking(
     rpc_index: usize,
     clients: Clients,
     cache: Cache,
+    timestamp_cache: TimestampCache,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut task_set = JoinSet::new();
 
     for para in registered_paras() {
         let clients_clone = Arc::clone(&clients);
         let cache_clone = Arc::clone(&cache);
+        let timestamp_cache_clone = Arc::clone(&timestamp_cache);
         task_set.spawn(async move {
-            track_weight_consumption(para, rpc_index, clients_clone, cache_clone).await;
+            track_weight_consumption(para, rpc_index, clients_clone, cache_clone, timestamp_cache_clone).await;
         });
     }
 
-    // Wait for all tasks to finish
     while let Some(res) = task_set.join_next().await {
         if let Err(e) = res {
             log::error!("A tracking task encountered an error: {:?}", e);
@@ -172,12 +153,12 @@ async fn start_tracking(
     Ok(())
 }
 
-// Track weight consumption for each parachain and broadcast updates to SSE clients
 async fn track_weight_consumption(
     para: Parachain,
     rpc_index: usize,
     clients: Clients,
     cache: Cache,
+    timestamp_cache: TimestampCache,
 ) {
     let Some(rpc) = para.rpcs.get(rpc_index) else {
         log::error!(
@@ -188,15 +169,11 @@ async fn track_weight_consumption(
         return;
     };
 
-    log::info!(
-        "{}-{} - Starting to track consumption.",
-        para.relay_chain,
-        para.para_id
-    );
+    log::info!("{}-{} - Starting to track consumption.", para.relay_chain, para.para_id);
     let result = OnlineClient::<PolkadotConfig>::from_url(rpc).await;
 
     if let Ok(api) = result {
-        if let Err(err) = track_blocks(api, para.clone(), clients, cache).await {
+        if let Err(err) = track_blocks(api, para.clone(), clients, cache, timestamp_cache).await {
             log::error!(
                 target: LOG_TARGET,
                 "{}-{} - Failed to track new block: {:?}",
@@ -221,13 +198,9 @@ async fn track_blocks(
     para: Parachain,
     clients: Clients,
     cache: Cache,
+    timestamp_cache: TimestampCache,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    log::info!(
-        target: LOG_TARGET,
-        "{}-{} - Subscribing to finalized blocks",
-        para.relay_chain,
-        para.para_id
-    );
+    log::info!("{}-{} - Subscribing to finalized blocks", para.relay_chain, para.para_id);
 
     let mut blocks_sub = api
         .blocks()
@@ -236,34 +209,36 @@ async fn track_blocks(
         .map_err(|_| "Failed to subscribe to finalized blocks")?;
 
     while let Some(Ok(block)) = blocks_sub.next().await {
-        note_new_block(
-            api.clone(),
-            para.clone(),
-            block,
-            clients.clone(),
-            cache.clone(),
-        )
-        .await?;
+        note_new_block(api.clone(), para.clone(), block, clients.clone(), cache.clone(), timestamp_cache.clone()).await?;
     }
 
     Ok(())
 }
 
-// Collect consumption data for each finalized block and broadcast to SSE clients
 async fn note_new_block(
     api: OnlineClient<PolkadotConfig>,
     para: Parachain,
     block: Block<PolkadotConfig, OnlineClient<PolkadotConfig>>,
     clients: Clients,
     cache: Cache,
+    timestamp_cache: TimestampCache,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let block_number = block.header().number;
-    let extrinsics = block.extrinsics().await?;
-    let extrinsics_num = extrinsics.len();
-
+    let extrinsics_num = block.extrinsics().await?.len();
     let authorities_num = authorities_num(&api, block.hash()).await?;
     let timestamp = timestamp_at(api.clone(), block.hash()).await?;
     let consumption = weight_consumption(api, block_number).await?;
+    let relay_chain = para.relay_chain.clone();
+
+    let block_time_seconds = {
+        let mut cache = timestamp_cache.write().await;
+        if let Some(previous_timestamp) = cache.insert(para.para_id, timestamp) {
+            let difference = (timestamp - previous_timestamp) as f64 / 1000.0;
+            Some(difference)
+        } else {
+            None
+        }
+    };
 
     let consumption_update = ConsumptionUpdate {
         para_id: para.para_id,
@@ -272,42 +247,37 @@ async fn note_new_block(
         extrinsics_num,
         authorities_num,
         timestamp,
+        block_time_seconds,
         ref_time: RefTime {
             normal: consumption.ref_time.normal,
             operational: consumption.ref_time.operational,
             mandatory: consumption.ref_time.mandatory,
         },
         proof_size: ProofSize {
-            normal: consumption.proof_size.normal,
-            operational: consumption.proof_size.operational,
-            mandatory: consumption.proof_size.mandatory,
+            normal: consumption.ref_time.normal,
+            operational: consumption.ref_time.operational,
+            mandatory: consumption.ref_time.mandatory,
         },
         total_proof_size: consumption.total_proof_size,
     };
 
     let consumption_json = serde_json::to_string(&consumption_update)?;
+    let event_id = format!("{}-{}-{}", relay_chain, para.para_id, block_number);
 
-    // Store the latest message in the cache
     {
         let mut cache_write_guard = cache.write().await;
         cache_write_guard.insert(para.para_id, consumption_update.clone());
     }
 
-    // Clone the list of clients while holding the read lock, then release it.
-    let client_list_snapshot;
-    {
+    let client_list_snapshot = {
         let clients_read_guard = clients.read().await;
-        client_list_snapshot = clients_read_guard.clone(); // Cloning the UnboundedSender instances
-    }
+        clients_read_guard.clone()
+    };
 
     let mut disconnected_clients = Vec::new();
-
-    // Now broadcast to the cloned list
     for (i, client) in client_list_snapshot.iter().enumerate() {
-        let event_id = format!("{}-{}", para.para_id, block_number);
-        // Create the SSE event with custom event type and ID
         let sse_event = Event::default()
-            .id(event_id)
+            .id(event_id.clone())
             .event("consumptionUpdate")
             .data(consumption_json.clone());
         if client.send(Ok(sse_event)).is_err() {
@@ -316,7 +286,6 @@ async fn note_new_block(
         }
     }
 
-    // Remove disconnected clients after the broadcast
     remove_disconnected_clients(Arc::clone(&clients), disconnected_clients).await;
 
     Ok(())
@@ -335,7 +304,6 @@ async fn remove_disconnected_clients(clients: Clients, disconnected_clients: Vec
 
     match timeout(Duration::from_secs(10), clients.write()).await {
         Ok(mut clients_write_guard) => {
-            log::debug!("Write lock acquired at {:?}", std::time::Instant::now());
             for &i in disconnected_clients.iter().rev() {
                 clients_write_guard.remove(i);
             }
@@ -354,7 +322,39 @@ async fn remove_disconnected_clients(clients: Clients, disconnected_clients: Vec
     }
 }
 
-// Fetch weight consumption data for the given block
+async fn authorities_num(
+    api: &OnlineClient<PolkadotConfig>,
+    block_hash: H256,
+) -> Result<usize, Box<dyn std::error::Error>> {
+    let storage_query = polkadot::storage().session().validators();
+    let result = api.storage().at(block_hash).fetch(&storage_query).await;
+
+    match result {
+        Ok(Some(validators)) => Ok(validators.len()),
+        Ok(None) => Ok(0),
+        Err(e) => {
+            log::debug!("Failed to fetch session.validators: {:?}. Assuming 0 authorities.", e);
+            Ok(0)
+        }
+    }
+}
+
+async fn timestamp_at(
+	api: OnlineClient<PolkadotConfig>,
+	block_hash: H256,
+) -> Result<Timestamp, Box<dyn std::error::Error>> {
+	let timestamp_query = polkadot::storage().timestamp().now();
+
+	let timestamp = api
+		.storage()
+		.at(block_hash)
+		.fetch(&timestamp_query)
+		.await?
+		.ok_or("Failed to query timestamp")?;
+
+	Ok(timestamp)
+}
+
 async fn weight_consumption(
     api: OnlineClient<PolkadotConfig>,
     block_number: u32,
@@ -382,7 +382,6 @@ async fn weight_consumption(
     let operational_proof_size = weight_consumed.operational.proof_size;
     let mandatory_proof_size = weight_consumed.mandatory.proof_size;
 
-    // Calculate the total proof size
     let total_proof_size = normal_proof_size + operational_proof_size + mandatory_proof_size;
 
     let consumption = WeightConsumption {
@@ -404,51 +403,4 @@ async fn weight_consumption(
 
     Ok(consumption)
 }
-async fn authorities_num(
-    api: &OnlineClient<PolkadotConfig>,
-    block_hash: subxt::utils::H256,
-) -> Result<usize, Box<dyn std::error::Error>> {
-    // Build the storage query for session.validators
-    let storage_query = polkadot::storage().session().validators();
 
-    // Attempt to fetch the list of validators at the given block hash
-    let result = api
-        .storage()
-        .at(block_hash)
-        .fetch(&storage_query)
-        .await;
-
-    match result {
-        Ok(Some(validators)) => {
-            // Return the number of validators
-            Ok(validators.len())
-        }
-        Ok(None) => {
-            // Storage item exists but no validators found
-            Ok(404404)
-        }
-        Err(e) => {
-            // Log a warning and return 0
-            log::debug!(
-                "Failed to fetch session.validators: {:?}. Assuming 0 authorities.",
-                e
-            );
-            Ok(404404)
-        }
-    }
-}
-async fn timestamp_at(
-	api: OnlineClient<PolkadotConfig>,
-	block_hash: H256,
-) -> Result<Timestamp, Box<dyn std::error::Error>> {
-	let timestamp_query = polkadot::storage().timestamp().now();
-
-	let timestamp = api
-		.storage()
-		.at(block_hash)
-		.fetch(&timestamp_query)
-		.await?
-		.ok_or("Failed to query timestamp")?;
-
-	Ok(timestamp)
-}
